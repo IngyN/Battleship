ec2c1aad577bf53c7b3510ac444f1bd9f0ef587c
AliaMYH <aliahassan95@aucegypt.edu>
2014-12-29 13:24:42 +0200

    Created a stats header and cpp

---
+++ battleeee/battleeee/Stats.cpp
@@ -0,0 +1,9 @@
+//
+//  Stats.cpp
+//  battleeee
+//
+//  Created by Alia Hassan  on 12/29/14.
+//  Copyright (c) 2014 Ingy. All rights reserved.
+//
+
+#include "Stats.h"
---
+++ battleeee/battleeee/Stats.h
@@ -0,0 +1,14 @@
+//
+//  Stats.h
+//  battleeee
+//
+//  Created by Alia Hassan  on 12/29/14.
+//  Copyright (c) 2014 Ingy. All rights reserved.
+//
+
+#ifndef __battleeee__Stats__
+#define __battleeee__Stats__
+
+#include <stdio.h>
+
+#endif /* defined(__battleeee__Stats__) */

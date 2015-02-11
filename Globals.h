fc4f9dd4fe6bb161e24af821ae1bd5171a956dd6
IngyN <ingyesa@aucegypt.edu>
2015-02-11 18:00:56 +0200

    globals.h

---
+++ Globals.h
@@ -0,0 +1,18 @@
+//
+//  Globals.h
+//  battleeee
+//
+//  Created by Ingy on 2/5/15.
+//  Copyright (c) 2015 Ingy. All rights reserved.
+//
+
+#ifndef battleeee_Globals_h
+#define battleeee_Globals_h
+
+#define gridCellSize 38
+#define oppGridXOffset 470
+#define oppGridYOffset 60
+#define plGridXOffset 60
+#define plGridYOffset 60
+
+#endif
